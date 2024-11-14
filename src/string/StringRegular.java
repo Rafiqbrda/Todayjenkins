95447f1cf600fc0df458d533e4e5cae4edc29982
Kam@yahoo.com <San@yahoo.com>
2024-11-14 15:15:50 -0600

    First JENKINS DEMO

---
+++ src/string/StringRegular.java
@@ -0,0 +1,19 @@
+package string;
+
+public class StringRegular {
+	
+	
+	
+	
+	public static void main(String[] args) {
+		
+		
+		String str = new String("Dhaka");
+		
+		
+		System.out.println(str);
+		
+		
+	}
+
+}
